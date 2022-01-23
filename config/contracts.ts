5f57cd760463bb2f41a6e845fdc1de02642ff1f3
saurabhburade <sburade3@gmail.com>
2022-01-23 22:02:43 +0530

    Chore : Add ABIs & providers

---
+++ config/contracts.ts
@@ -0,0 +1,8 @@
+export default {
+    usersFactory: {
+        4002:"",
+        250:"",
+        97:"0xA9e21cfC912b76cB6EA94c2D90047b3c65B37136",
+    }
+
+}
--- pages/register.tsx
+++ pages/register.tsx
@@ -1,27 +1,64 @@
 import React, { ReactElement } from "react";
 import { Header } from "../components/Header/Header";
 import { create } from "ipfs-http-client";
+import { useWeb3React } from "@web3-react/core";
+import { ethers } from "ethers";
+import userFactoryABI from "../config/abis/usersFactoryABI.json";
+import UncheckedJsonRpcSigner from "../utils/signer";
 
 interface Props {}
 
-export default function register({ }: Props): ReactElement {
-   const [fileUrl, updateFileUrl] = React.useState(``);
-   async function onChange(e) {
-     const file = e.target.files[0];
-     try {
-       const added = await client.add(file);
-       const url = `https://dweb.link/ipfs/${added.path}`;
-       console.log({ added });
+// account is optional
+export function getProviderOrSigner(library, account) {
+  return account
+    ? new UncheckedJsonRpcSigner(library.getSigner(account))
+    : library;
+}
 
-       updateFileUrl(url);
-     } catch (error) {
-       console.log("Error uploading file: ", error);
-     }
-   }
+export default function register({}: Props): ReactElement {
+  const { account, library } = useWeb3React();
+
+  const web3Provider = new ethers.providers.JsonRpcProvider(
+    "https://data-seed-prebsc-1-s1.binance.org:8545/"
+  );
+  console.log({ library, web3Provider }, web3Provider.getSigner());
+  const contract = new ethers.Contract(
+    "0x261725549A8EA98F934B6bf8d3541e737Fb45d74",
+    userFactoryABI,
+    library?.getSigner(account)
+  );
+  const contractWithSigner = contract.connect(web3Provider.getSigner(account));
+  const [fileUrl, updateFileUrl] = React.useState(``);
+  const [userName, setuserName] = React.useState(``);
+  async function onFileChange(e) {
+    const file = e.target.files[0];
+    try {
+      const added = await client.add(file);
+      const url = `https://dweb.link/ipfs/${added.path}`;
+      console.log({ added });
+
+      updateFileUrl(added.path);
+    } catch (error) {
+      console.log("Error uploading file: ", error);
+    }
+  }
+  const handleRegister = async () => {
+    console.log(
+      "handleRegister",
+      contractWithSigner.createUser,
+      library?.getSigner(account),
+      web3Provider.getSigner(account)
+    );
+    const txn = await contractWithSigner.createUser("bob", "ipfshashhere");
+    console.log(txn);
+    if (fileUrl.trim() && userName.trim() && account) {
+      console.log({ contractWithSigner });
+    }
+  };
   return (
     <div>
       <Header />
-      <div className="bg-gray-800 p-10 h-screen flex flex-col items-center justify-center">
+      <div className="flex flex-col items-center justify-center h-screen p-10 bg-gray-800">
         <div className="w-6/12 my-5 ">
           <div className="my-5 ">
             <label
@@ -35,30 +72,32 @@ export default function register({ }: Props): ReactElement {
               type="text"
               name="userName"
               id="userName"
-              className="focus:ring-indigo-500 focus:border-indigo-500 block w-full py-5 pl-7 pr-12 sm:text-sm border-gray-300 rounded-md"
+              className="block w-full py-5 pr-12 border-gray-300 rounded-md focus:ring-indigo-500 focus:border-indigo-500 pl-7 sm:text-sm"
               placeholder="Enter Preferred Username"
+              onChange={(e) => setuserName(e.target.value)}
             />
           </div>
           <div>
             <label className="block text-xl font-medium text-gray-200">
               Photo
             </label>
-            <div className="mt-1 flex items-center my-5 ">
-              <span className="inline-block h-12 w-12 rounded-full overflow-hidden bg-gray-100">
+            <div className="flex items-center my-5 mt-1 ">
+              <span className="inline-block w-12 h-12 overflow-hidden bg-gray-100 rounded-full">
                 <svg
-                  className="h-full w-full text-gray-300"
+                  className="w-full h-full text-gray-300"
                   fill="currentColor"
                   viewBox="0 0 24 24"
                 >
                   <path d="M24 20.993V24H0v-2.996A14.977 14.977 0 0112.004 15c4.904 0 9.26 2.354 11.996 5.993zM16.002 8.999a4 4 0 11-8 0 4 4 0 018 0z" />
                 </svg>
               </span>
-              <input type="file" onChange={onChange} />
+              <input type="file" onChange={onFileChange} />
             </div>
-              {fileUrl && <img src={fileUrl} width="600px" />}{" "}
+            {fileUrl && <img src={fileUrl} width="600px" />}{" "}
             <button
               type="button"
-              className=" bg-gray my-5 py-2 px-3 border border-gray-300 rounded-md shadow-sm text-sm leading-4 font-medium text-gray-200 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+              onClick={handleRegister}
+              className="px-3 py-2 my-5 text-sm font-medium leading-4 text-gray-200 border border-gray-300 rounded-md shadow-sm bg-gray hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
             >
               Register
             </button>
@@ -69,11 +108,7 @@ export default function register({ }: Props): ReactElement {
   );
 }
 
-
-
-
 const client = create("https://ipfs.infura.io:5001/api/v0");
-
 
 export const IpfsFileUpload = (props: Props) => {
   const [fileUrl, updateFileUrl] = useState(``);
@@ -94,7 +129,7 @@ export const IpfsFileUpload = (props: Props) => {
       <div className="App">
         <h1>IPFS Example</h1>
         <input type="file" onChange={onChange} />
-        {fileUrl && <img src={fileUrl} width="600px" />}
+        {fileUrl && <img src={fileUrl} alt={fileUrl} width="600px" />}
       </div>
     </div>
   );
